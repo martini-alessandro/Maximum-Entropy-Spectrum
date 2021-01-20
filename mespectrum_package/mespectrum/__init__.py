f6a95e972b83ffc743724d34090eb38d6186f1ef
stefanoschmidt1995 <stefanoschmidt1995@gmail.com>
2021-01-20 10:37:57 +0100

    Copied package files

--- mespectrum_package/mespectrum/__init__.py
+++ mespectrum_package/mespectrum/__init__.py
@@ -1,6 +1,6 @@
 """
-mesa
-====
+mespectrum
+==========
 
 A nice package for Maximum Entropy Spectral Analysis.
 Include short description here: it will appear when typing help(mesa)
@@ -11,10 +11,7 @@ import sys
 import numpy as np
 import warnings
 
-#Stefano: put as everywhere as possible the dimensions of the variables we are using?
-#Stefano: make the help of every function?
 #Stefano: should the sampling rate be given at initialization? Or is it fine like it is today?
-#Stefano: why don't we merge generate Time series in the mesa class? So we have a single file
 
 #############DEBUG LINE PROFILING
 try:
@@ -45,6 +42,18 @@ except:
 class optimizer:
 
     def __init__(self, method):
+        """
+        Implements various method to choose the best recursive order for Burg's Algorithm
+        Avilable methods are "FPE", "OBD", "CAT", "AIC". The most representative order is 
+        chosen to be the one that minimized the related function. 
+        Parameters
+        ----------
+        method : 'str'
+            Selects the method to be used to estimate the best order between "FPE",
+            "OBD", "CAT", "AIC"
+
+
+        """
         self.method = method
 
     def __call__(self, *args): #Stefano: what are args? We should specify them and call them by name...
@@ -62,23 +71,69 @@ class optimizer:
             raise ValueError("{} is not a an available method! Valid choices are 'FPE', 'AIC', 'CAT', 'OBD' and 'Fixed'.".format(self.method))
     
     def _FPE(self, P, N, m):
+        """
+        Implements Akaike Final prediction Error to estimate the recursive 
+        order 
+        
+        Parameters
+        ----------
+        P : 'np.float'
+            The estimate of the variance for the white noise component.
+        N : 'np.int'
+            The length of the dataset.
+        m : 'np.int'
+            The recursive order.
+
+        Returns
+        -------
+        'np.float'
+            The value of FPE optimizer.
+
+        """
         return P[-1] * (N + m + 1) / (N - m - 1)
     
     def _AIC(self, P, N, m):
         """
-        description
+        Implements Akaike information criterion to estimate the recursive 
+        order 
         
-        params:
-        P: `np.float` power
-        ...
-        
-        return
-        the value returned
-        
+        Parameters
+        ----------
+        P : 'np.float'
+            The estimate of the variance for the white noise component.
+        N : 'np.int'
+            The length of the dataset.
+        m : 'np.int'
+            The recursive order.
+
+        Returns
+        -------
+        'np.float'
+            The value of AIC optimizer.
+
         """
         return np.log(P[-1]) + (2 * m) / N
     
     def _CAT(self, P, N, m):
+        """
+        Implements Parzen's criterion on autoregressive transfer function
+        to estimate the recursive order 
+        
+        Parameters
+        ----------
+        P : 'np.float'
+            The estimate of the variance for the white noise component.
+        N : 'np.int'
+            The length of the dataset.
+        m : 'np.int'
+            The recursive order.
+
+        Returns
+        -------
+        'np.float'
+            The value of CAT optimizer.
+
+        """
         if m == 0:
             return np.inf
         P = np.array(P[1:])
@@ -87,6 +142,28 @@ class optimizer:
         return 1 / (N * PW_k.sum())- (1 / PW_k[-1])
     
     def _OBD(self, P, a_k, N, m):
+        """
+        Implement Rao's Optimum Bayes Decision rule to estimate the recursive
+        order
+
+        Parameters
+        ----------
+         P : 'np.float'
+            The estimate of the variance for the white noise component.
+        a_k : 'np.array'
+            The values for the final prediction error coefficients for the 
+            considered recursive order.
+        N : 'np.int'
+            The length of the dataset.
+        m : 'np.int'
+            The recursive order.
+
+        Returns
+        -------
+        TYPE
+            The value of OBD optimizer.
+
+        """
         P_m = P[-1]
         P = np.array(P[:-1])
         return (N - m - 2)*np.log(P_m) + m*np.log(N) + np.log(P).sum() + (a_k**2).sum()
@@ -96,10 +173,11 @@ class optimizer:
 
 class MESA(object):
     """
-    description
+    Class the implement the reproduces the Maximum Entropy Spectrum of a given 
+    time-series. 
     
-    init: data: `np.ndarray` shape (N)
-    
+    init: data: `np.ndarray` shape (N,)
+    solve(): 
     """
     def __init__(self, data):
         """ 
@@ -108,11 +186,93 @@ class MESA(object):
         
         Parameters
         ----------
-            data: 'np.ndarray'       
-                Time series with power spectral density to be computed 
+        data: 'np.ndarray'       
+            Time series with power spectral density to be computed 
         """ 
         self.data = data
         self.N    = len(self.data)
+        self.P = None
+        self.a_k = None #If a_k and P are None, the model is not already fitted
+        self.optimization = None
+
+    def save(self,filename):
+        """
+        Save the class to file (if the spectral density analysis is already performed).
+        The output file can be used to load the class with method load()
+        File is a 1D array with the format: [P, a_k, optimization, data]. The header holds the shapes for each array
+        
+        Parameters
+        ----------
+        filename: `string`      
+            Name of the file to save the data at
+        """
+        if self.P is None or self.a_k is None:
+            raise RuntimeError("PSD analysis is not performed yet: unable to save the model. You should call solve() before saving to file") 
+        
+        to_save = np.concatenate([[self.P], self.a_k, self.optimization, self.data])
+        header = "(1,{},{},{})".format(len(self.a_k), len(self.optimization), len(self.data))
+        print(header)
+        np.savetxt(filename, to_save, header = header)
+        return
+        
+    def load(self,filename):
+        """
+        Load the class from a given file. The file shall be the same format produced by save().
+        
+        Parameters
+        ----------
+        filename: `string`      
+            Name of the file to load the data from
+        """
+        data = np.loadtxt(filename)
+        if data.ndim != 1:
+            raise ValueError("Wrong format for the file: unable to load the model")
+        
+            #reading the first line
+        with open(filename) as f:
+            first_line = f.readline()
+
+        shapes = eval(first_line.translate({ord('#'): None, ord(' '): None}))
+            #checking for the header
+        if not isinstance(shapes, tuple):
+            if len(shapes) != 4 or not np.all([isinstance(s, int) for s in shapes]):
+                raise ValueError("Wrong format for the header: unable to load the model")
+
+            #assigning values
+        self.P, self.a_k, self.optimization, self.data = np.split(data, np.cumsum(shapes)[:-1])
+
+        self.N = shapes[3]
+        
+        return
+        
+    def save_spectrum(self, filename, dt, frequencies = None):
+        """
+        Saves the power spectral density computed by the model to a txt file. The PSD is evaluated on a user given grid of frequencies. If None, a standard grid is used (as computed by np.fft.fftfreq).
+        The spectrum is saved as a 2D array: [f, PSD(f)]
+        
+        Parameters
+        ----------
+        filename: `string`      
+            Name of the file to save the PSD at
+
+        dt: `np.float`      
+            Sampling rate for the time series
+
+        frequencies: `np.ndarray`      
+            Frequencies to compute the PSD at. If None, a standard grid will be computed
+                
+        """
+        if isinstance(frequencies, np.ndarray):
+            PSD = self.spectrum(dt,frequencies)
+        elif frequencies is None:
+            PSD, frequencies = self.spectrum(dt)
+        else:
+            raise ValueError("Wrong type for input frequencies: expected \`np.ndarray\` or None, got {} instead".format(type(frequencies)))
+         
+        to_save  = np.column_stack([frequencies, PSD])
+        np.savetxt(filename, to_save, header = "dt = {} s".format(dt))
+        return
+        
         
     def _spectrum(self, dt, N):
         """
@@ -179,16 +339,18 @@ class MESA(object):
         """
         f_ny = .5 / dt 
         
-        if type(frequencies) == np.ndarray: 
+        if isinstance(frequencies, np.ndarray): 
             df = np.min(np.abs(np.diff(frequencies))) * 0.9
-            if np.max(frequencies) > f_ny: 
-                raise ValueError("Some of the required frequencies are higher than the Nyquist frequency: unable to continue")
+            if np.max(frequencies) > f_ny *1.01: 
+                warnings.warn("Some of the required frequencies are higher than the Nyquist frequency ({} Hz): a zero PSD is returned for f>Nyquist".format(f_ny))
         
-        if frequencies == None: N = self.N
+        if frequencies is None: N = self.N
         elif isinstance(frequencies, np.ndarray): N = int(2. * f_ny / df)
+        else: raise ValueError("Type of frequencies not understood: expected to be None or np.ndarray but given {} insted".format(type(frequencies)))
+
         spec, f_spec = self._spectrum(dt, N)
         
-        if frequencies == None: 
+        if frequencies is None: 
             return spec, f_spec   
 
         f_interp = np.interp(frequencies, f_spec[:int(N/2+0.5)], spec.real[:int(N/2+0.5)], left = 0., right = 0.)
@@ -249,7 +411,7 @@ class MESA(object):
         
         self.regularisation = regularisation
         self.early_stop = early_stop
-        if m == None:
+        if m is None:
             self.mmax = int(2*self.N/np.log(2.*self.N))
         else:
             self.mmax = m
@@ -316,7 +478,7 @@ class MESA(object):
             #Compute optimizer value for chosen method
             optimization.append( self._optimizer(P, a[-1], self.N, i + 1) )
             
-            	#checking if there is a minimum (every some iterations) if early_stop option is on
+                #checking if there is a minimum (every some iterations) if early_stop option is on
             if ((i % 200 == 0 and i !=0) or (i >= self.mmax-1)) and self.early_stop:
                 idx = np.argmin(optimization) + 1
                 if old_idx < idx:
@@ -330,6 +492,26 @@ class MESA(object):
         return P[idx], a[idx], np.array(optimization)
    
     def _updateCoefficients(self, a, g):
+        """
+        Updates the forward prediction error coefficients (needed to compute 
+        to compute the spectrum) from order i to order i + 1 
+        i + 1
+
+        Parameters
+        ----------
+        a : 'np.ndarray'
+            The i'th order forward prediction error coefficients. (Shape (i,))
+        g : 'np.ndarray'
+            Array used to update the forward prediction error (Shape (i+1, )).
+
+        Returns
+        -------
+        k : 'np.float'
+            The reflection coefficient.
+        aUpd : 'np.ndarray'
+            The (i+1)'th order forward predicition error coefficients (Shape (i+1,)).
+
+        """
         a = np.concatenate((a, np.zeros(1)))
         k = - (np.dot(a.conj(), g[::-1])) / (np.dot(a, g))
         aUpd = a + k * a[::-1].conj()
@@ -402,7 +584,7 @@ class MESA(object):
             _b = b + k * f
             #print('P: ', P, '\nak: ', a_k[-1])
             optimization.append(self._optimizer(P, a_k[-1], self.N, i + 1))
-            	#checking if there is a minimum (every some iterations) if early_stop option is on
+                #checking if there is a minimum (every some iterations) if early_stop option is on
             if ((i % early_stop_step == 0 and i !=0) or (i >= self.mmax-1)) and self.early_stop:
                 idx = np.argmin(optimization) + 1
                 if old_idx < idx:
@@ -416,34 +598,30 @@ class MESA(object):
         return P[idx], a_k[idx], optimization
     
     def _updatePredictionCoefficient(self, x, reflectionCoefficient):
+        """
+        Uses the Levinson recursion to update the prediction error coefficients
+
+        Parameters
+        ----------
+        x : 'np.ndarray'
+            The i'th order forward prediction error coefficients (Shape (i,)).
+        reflectionCoefficient : 'np.float'
+            The i'th order reflection coefficients, used to update the forward
+            prediction error coefficients via the solution of Levinson Recursion.
+
+        Returns
+        -------
+        'np.ndarray'
+            The updatet forward prediction error coefficients at order i + 1 (Shape (i + 1,)).
+
+        """
         new_x = np.concatenate((x, np.zeros(1)))
         return new_x + reflectionCoefficient * new_x[::-1]
     
-    def forecast(self, length, number_of_simulations, P = None):
-        #FIXME: can this be vectorized?
-        if P == None: P = self.P
-        p = self.a_k.size - 1 
-        coef = - self.a_k[1:][::-1]
-        future = [] 
-        for _ in range(number_of_simulations):
-            #sys.stderr.write('\r%f' %((_ + 1)/number_of_simulations))
-            predictions = self.data[-p:]            
-            for i in range(length):#FIXME: progress bar?
-               # sys.stderr.write('\r {0} of {1}'.format(i + 1, length))
-                prediction = predictions[-p:] @ coef +\
-                             np.random.normal(0, np.sqrt(P))
-#                while prediction < 0:
-#                    prediction = predictions[-p:] @ coef +\
-#                             np.random.normal(0, np.sqrt(P))
-                predictions = np.append(predictions, prediction)
-            future.append(predictions[p:])
-        #sys.stderr.write('\n')
-        return np.array(future)
-    
-    def forecast_vectorized(self, length, number_of_simulations, P = None): 
+    def forecast(self, length, number_of_simulations, P = None, include_data = False): 
         """
         Forecasting on the observed process for a total number of points given 
-        by length, to be performed as many times as number_of_simulations. 
+        by length. It computes number_of_simulations realization of the forecast time series.
         This method can only be used if a_k coefficients have been computed 
         already. Use solve method before forecasting. 
 
@@ -457,7 +635,10 @@ class MESA(object):
             
         P : 'np.float'
             Variance of white noise for the autoregressive process. 
-            Default is None and uses the estimate obtained with Burg's algorithm. 
+            Default is None and uses the estimate obtained with Burg's algorithm.
+            
+        include_data: `bool`
+            Whether to prepend to the output the input time series
 
         Returns
         -------
@@ -466,19 +647,60 @@ class MESA(object):
             process (Shape (number_of_simulations, length))
 
         """
-        if P == None: P = self.P 
+        if self.P is None or self.a_k is None:
+            raise RuntimeError("PSD analysis is not performed yet: unable to forecast the data. You should call solve() before forecasting")
+        if P is None: P = self.P 
         p = self.a_k.size - 1 
-        #predictions = np.zeros((number_of_simulations, p + length))
         predictions = np.zeros((number_of_simulations, p + length))
         predictions[:,:p] = self.data[-p:]
-        coef = self.a_k[1:][::-1]
+        coef = - self.a_k[1:][::-1]
         for i in range(length): 
             sys.stderr.write('\r {0} of {1}'.format(i + 1, length))
             predictions[:, p + i] = predictions[:, i: p + i] @ coef +\
                          np.random.normal(0, np.sqrt(P), size = number_of_simulations)
+        sys.stderr.write('\n')
+        if not include_data:
+            return predictions[:,p:]
         return predictions
 
+    def generate_noise(self, T, sampling_rate = 1., fmin = None, fmax = None, N_series = 1):
+        """
+        Generate some noise starting from a mesa object. The noise generated has the same features as the data given in input to the mesa.
+            
+        Parameters
+        ----------
+                T: `float`                  Length (in seconds) of the signal to generate
+                sampling_rate: `np.float`   Sampling rate for the time series to generate
+                fmin: `float`               Minimum frequency in the signal (if None, is equal to zero)
+                fmax: `float`               Maximum frequency in the signal (if None, Nyquist frequency is used: f_Ny = 0.5*sampling_rate)
+                N_series `float`            Number of time series to generate
+            
+            Returns:
+            --------
+                times: `np.ndarray`                  time grid at which the noise time series is evaluated at
+                times_series: `np.ndarray`           time series (shape (N_series, sampling_rate*T) )
+                frequencies: `np.ndarray`            frequency grid at which the noise frequency series is evaluated at
+                frequency_series: `np.ndarray`       frequency series from which the times_series is computed (shape (N_series, sampling_rate*T) )
+                psd: `np.ndarray`                    PSD of the model, including both positive and negative frequencies
+        """
+        df      = 1 / T
+        N       = int(sampling_rate * T)
+        times   = np.linspace(0., T , N) 
+        if fmin == None: fmin = 0
+        if fmax == None: fmax = (N / 2) / T
+        # filter out the bad bits
+        kmin = np.int(fmin/df)
+        kmax = np.int(fmax/df) + 1
+        
+        # generate the FD noise
+        frequencies = df * np.linspace(kmin, kmax, int(N / 2 + 1)) #(D,) #df * N / 2 is Ny frequency, + 1 needed because arange cuts last term
+        psd = self.spectrum(1/sampling_rate, frequencies)
 
+        sigma = np.sqrt(psd /  df * .5) #(D,)
+        phi = np.random.uniform(0, 2 * np.pi, len(sigma))
+        frequency_series = np.einsum('ij,j -> ij',np.random.normal(0, 1, (N_series,len(sigma))) + 1j * np.random.normal(0, 1, (N_series,len(sigma)) ), sigma) #(N_series,D)
+          
+        # inverse FFT to return the TD strain
+        time_series = np.fft.irfft(frequency_series) * df * N ##(N_series, N )
+        return times, np.squeeze(time_series), frequencies, np.squeeze(frequency_series), psd
 
-#FIXME: methods to save and load psd and AR coefficients in various formats, depending on the necessary application
-#FIXME: save/load class as pickle
