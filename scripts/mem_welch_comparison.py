ff2cbb778c1ca3adb5a8efe285e9e0b21799e253
martini-alessandro <martini.alessandr@gmail.com>
2021-02-17 19:54:08 +0100

    White noise and Ligo noise comparison

--- scripts/mem_welch_comparison.py
+++ scripts/mem_welch_comparison.py
@@ -18,12 +18,6 @@ from scipy.interpolate import interp1d
 
 
 
-def compute_spectra(time_series, dt):
-    welch_frequency, welch_spectrum = [], []
-    MESA_frequency, MESA_spectrum = [], [] 
-    for i in range(len(time_series)): 
-        None 
-
 def relative_error(real, estimate):
     return np.abs(real - estimate) / real 
 
@@ -32,17 +26,15 @@ def relative_error(real, estimate):
 if __name__ == '__main__': 
     #Sampling variables 
     dt = 1. / 4096
-    times = np.array([1, 5, 10, 100, 1000])
+    times = np.array([1, 5, 10, 100])#, 1000])
     N = times // dt 
-    segment_duration = np.array([521, 1024, 2048, 8192, 32768]) 
+    segment_length = np.array([521, 1024, 2048, 8192])  #, 32768])
     Ny = 0.5 / dt 
     
     #Generate normal PSD and import Ligo PSD 
-    wnoise_frequency = np.arange(0, N[0] // 2 + 1) / times[0]
-    wnoise_spectrum = np.repeat(1, wnoise_frequency.size) * dt 
+    wnoise_frequency, wnoise_spectrum = np.arange(0, N[0] // 2 + 1) / times[0], np.repeat(1, N[0] // 2 + 1) * dt
     ligo_frequency, ligo_spectrum = np.loadtxt('LIGO-P1200087-v18-AdV_DESIGN_psd.dat', unpack=True)     
-    ligo_interp = interp1d(ligo_frequency, ligo_spectrum)
-    #Lists containing all simulated arrays, frequencies and psds 
+    #Generating Noise 
     print('Generating white noise series')
     Wtime, Wtime_series, Wfrequency, Wfrequency_series, Wpsd = generate_data(wnoise_frequency,
                                                                         wnoise_spectrum,
@@ -53,6 +45,36 @@ if __name__ == '__main__':
                                                                              ligo_spectrum,
                                                                              times[-1],
                                                                              1 / dt)
+    w_interp = interp1d(wnoise_frequency, wnoise_spectrum)   
+    l_interp = interp1d(ligo_frequency, ligo_spectrum, fill_value = 'extrapolate')    
+    M = MESA()
+    
+
+    for i, n in enumerate(segment_length):
+        #White Noise
+        w_welchFreq, w_welchSpectrum = welch.psd(Wtime_series[:int(N[i])], 1 / dt, n * dt)
+        M.solve(Wtime_series[:int(N[i])])
+        w_mesaSpectrum, w_mesaFreq = M.spectrum(dt)
+        
+        fig, ax = plt.subplots() 
+        ax.loglog(w_welchFreq[ : n // 2], w_welchSpectrum[: n // 2], color = 'blue')
+        ax.loglog(w_mesaFreq[ : int(N[i] // 2)], w_mesaSpectrum[: int(N[i] // 2)], color = 'r')
+        ax.loglog(wnoise_frequency, wnoise_spectrum, '--', color = 'k')
+        ax.set_title('White Noise reconstrcution with {} points'.format(N[i]))
+        #Ligo Noise
+        l_welchFreq, l_welchSpectrum = welch.psd(Ltime_series[:int(N[i])], 1 / dt, n * dt )
+        M.solve(Ltime_series[:int(N[i])])
+        l_mesaSpectrum, l_mesaFreq = M.spectrum(dt)
+        
+        fig2, ax2 = plt.subplots()
+        ax2.loglog(l_welchFreq[: n // 2], l_welchSpectrum[: n // 2], color = 'blue')
+        ax2.loglog(l_mesaFreq[: int(N[i] // 2)], l_mesaSpectrum[:int(N[i] // 2)], color = 'red')
+        ax2.loglog(ligo_frequency, ligo_spectrum, '--', color = 'k')
+        ax2.set_title('Ligo Noise reconstrcution with {} points'.format(N[i]))
+        ax2.set_xlim(10, 2048)
+        
+
+    
     
     
  
