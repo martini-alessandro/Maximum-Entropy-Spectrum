6fc534577f64f76b3ff912875f162c73e9c43b07
stefanoschmidt1995 <stefanoschmidt1995@gmail.com>
2021-03-23 10:41:56 +0100

    Added forecasting in temperatures

--- scripts/plot_climate_data.py
+++ scripts/plot_climate_data.py
@@ -10,6 +10,7 @@ plot_dir = '../paper/Images/climate_plots/'
 
 data = np.loadtxt('../climate_data/data_@1h_len271009_1989-2020.dat')
 
+	#spectrum computation
 srate = 1./3600 #in Hz 1/(1 hours)
 M_CAT = memspectrum.MESA()
 M_FPE = memspectrum.MESA()
@@ -27,12 +28,39 @@ f_CAT = f_CAT[:int(N/2)]
 
 print(len(M_CAT.a_k),len(M_FPE.a_k))
 
+	#Forecasting
+N_forecast = 24*2*365
+
+if False:
+	forecast = M_CAT.forecast(data[:len(M_CAT.a_k)],N_forecast, 100, verbose = True)
+	np.savetxt('forecast_climate_data.dat', forecast)
+else:
+	forecast = np.loadtxt('forecast_climate_data.dat')
+l,m,h = np.percentile(forecast,[5,50,95],axis = 0)
+true = data[len(M_CAT.a_k):len(M_CAT.a_k)+N_forecast]
+
+	
+	#plot forecasting
+fig = init_plotting()
+ax = fig.gca()
+
+t_grid = np.linspace(0,N_forecast/24.,N_forecast)
+ax.plot(t_grid, (true-m), c = 'b', zorder = 1)
+ax.fill_between(t_grid, true-h, true-l, color = 'r',alpha = 0.3, zorder = 0)
+#ax.plot(t_grid, (true-m)/(h-l), c = 'b', zorder = 1)
+#ax.plot(np.linspace(0,N_forecast,N_forecast), true-h, c = 'r')
+#ax.plot(np.linspace(0,N_forecast,N_forecast), true- l, c = 'r')
+ax.set_xlabel("Time (days)")
+ax.set_ylabel(r"$T - T_{forecast} (K)$")
+plt.tight_layout()
+plt.savefig(plot_dir+"forecast_accuracy.pdf")
+
 	#plot spectrum
 fig = init_plotting()
 ax = fig.gca()
 
 unit_shift = 3600*24 #frequency is made 1/day
-ax.loglog(f_CAT * unit_shift , spec_CAT, label = "CAT", c = 'k', zorder = 1)
+ax.loglog(f_CAT * unit_shift , spec_CAT, label = "CAT", c = 'b', zorder = 1)
 ax.loglog(f_FPE * unit_shift , spec_FPE, label = "FPE", c = 'r', zorder = 2)
 
 ax.axvline(1./(3600.*24.*365.)*unit_shift,lw = .5, c = 'b', ls = '--', zorder = 0)
