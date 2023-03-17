6c285703cab308f7eae73b512ca30646aa40314a
stefanoschmidt1995 <stefanoschmidt1995@gmail.com>
2023-03-17 12:26:28 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+import os
+
+try:
+	from sphinx.setup_command import BuildDoc
+	cmdclass = {'build_sphinx': BuildDoc} #to build with sphinx
+except ImportError:
+	if sys.argv[1] == 'build_sphinx': warnings.warn("sphinx module not found: impossibile to build the documents")
+	pass
+
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+#setuptools.find_packages()
+
+setup(
+    name='memspectrum',
+    version='1.3.0',
+    author='Alessandro Martini',
+    author_email='martini.alessandr@gmail.com',
+    packages=['memspectrum'],
+    package_dir = {'memspectrum':'memspectrum'},
+    url="https://github.com/martini-alessandro/Maximum-Entropy-Spectrum/",
+    license='CC by 4.0',
+    description='maximum entropy spectral analysis',
+    long_description=readme(),
+    include_package_data = True,
+#    package_data={'memspectrum': get_package_data()},
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+	long_description_content_type = 'text/x-md',
+	command_options={
+        'build_sphinx': {
+            'source_dir': ('setup.py', 'docs'),
+            'build_dir': ('setup.py', 'docs/__build'),
+            }},
+)
+
