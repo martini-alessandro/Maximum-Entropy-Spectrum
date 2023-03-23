0b7493b5691edd94e7157f1c4e3c9dd19d086970
stefanoschmidt1995 <stefanoschmidt1995@gmail.com>
2023-03-23 10:37:40 -0400

    Updated peak ratio study

--- dev/peak_ratio.py
+++ dev/peak_ratio.py
@@ -3,21 +3,24 @@ from memspectrum import MESA
 import matplotlib.pyplot as plt
 import scipy.signal
 
-n = 9996 # number of data points
-T = 1      # total duration of the data in second
+n = 99960 # number of data points
+T = 10      # total duration of the data in second
 fs = n/T  # sample rate
 # Defining the sin wave
 f1 = 1000
-f2 = 300
+f2 = 345
+f3 = 400
 a1 = 100
 a2 = 10
+a3 = 0
 
 # Generating some signal.
-t = np.linspace(0,1,n)
-s1 = a1 * np.sin(2 * np.pi * f1 * t) + a2 * np.sin(2 * np.pi * f2 * t)  + np.random.normal(0, scale=1, size=n)
+t = np.linspace(0,T,n)
+s1 = a1 * np.sin(2 * np.pi * f1 * t) + a2 * np.sin(2 * np.pi * f2 * t) + a3 * np.sin(2 * np.pi * f3 * t)
+s1 += np.random.normal(0, scale=20, size=n)
 
 m = MESA()
-m.solve(s1, method = 'Standard', optimisation_method = 'VM')
+m.solve(s1, method = 'Standard', optimisation_method = 'FPE')
 
 psd_welch = scipy.signal.welch(s1, fs, window='tukey')
 
